d72c3b72c82ddb20154214c73fb529e722cc8449
Harry Sharma <harrysharma1066@gmail.com>
2023-11-02 15:04:04 +0000

    rename socials to scraper

--- scraper/instagram.py
+++ scraper/instagram.py
@@ -6,10 +6,8 @@ from PIL import Image
 class Instagram(SocialMedia):
     def __init__(self):
         super().__init__(platform_url='https://www.instagram.com',platform_name='Instagram')
-        
-        
-        
-    def get_followers(self,username:str):
+               
+    def get_follower_count(self,username:str):
         url = f'{self.platform_url}/{username}/?__a=1&__d=1'
         response = requests.get(url)
         if response.ok:
@@ -18,7 +16,7 @@ class Instagram(SocialMedia):
         else:
             self.http_error(response.status_code)
     
-    def get_following(self,username:str):
+    def get_following_count(self,username:str):
         url = f'{self.platform_url}/{username}/?__a=1&__d=1'
         response = requests.get(url)
         if response.ok:
---
+++ tests/instagram_tests.py
@@ -0,0 +1,10 @@
+import pytest
+from scraper.instagram import Instagram
+
+class TestInstagram:
+    
+    def test_get_follower_count(instagram):
+        instagram = Instagram()
+        assert instagram.get_follower_count('') == 'Undefined HTTP error'
+        
+        
