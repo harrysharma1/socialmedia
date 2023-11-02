5e1fda962a2df2faedd00ac9fc6413aea4565662
Harry Sharma <harrysharma1066@gmail.com>
2023-11-02 11:42:27 +0000

    add get follower and profile pic function

--- socials/instagram.py
+++ socials/instagram.py
@@ -1,17 +1,25 @@
 from socialmedia import SocialMedia
-import instaloader
+import requests
 
 class Instagram(SocialMedia):
     def __init__(self):
-        super().__init__(platform_url="https://www.instagram.com",platform_name="Instagram")
+        super().__init__(platform_url='https://www.instagram.com',platform_name='Instagram')
         
         
         
     def get_followers(self,username:str):
-        insta = instaloader.Instaloader()
-        profile = instaloader.Profile.from_username(insta.context,username=username)
-        return profile.followers
-    
-  
-                    
-  
+        url = f'{self.platform_url}/{username}/>?__a=1&__d=1'
+        response = requests.get(url)
+        if response.ok:
+            response = response.json()
+            return response['graphql']['user']['edge_followed_by']['count']
+ 
+        
+    def get_profile_pic(self,username:str):
+        url = f'{self.platform_url}/{username}/?__a=1&__d=1'
+        response = requests.get(url)
+        if response.ok:
+            response = response.json()
+            print(response['graphql']['user']['profile_pic_url_hd'])        
+               
+      
