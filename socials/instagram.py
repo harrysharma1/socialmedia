bc8720fbd88ddee75d129856ceb30857328cf627
Harry Sharma <harrysharma1066@gmail.com>
2023-11-02 12:08:05 +0000

    Add get following and http error messages

--- socials/instagram.py
+++ socials/instagram.py
@@ -8,18 +8,29 @@ class Instagram(SocialMedia):
         
         
     def get_followers(self,username:str):
-        url = f'{self.platform_url}/{username}/>?__a=1&__d=1'
+        url = f'{self.platform_url}/{username}/?__a=1&__d=1'
         response = requests.get(url)
         if response.ok:
             response = response.json()
-            return response['graphql']['user']['edge_followed_by']['count']
- 
-        
+            print(response['graphql']['user']['edge_followed_by']['count'])
+        else:
+            self.http_error(response.status_code)
+    
+    def get_following(self,username:str):
+        url = f'{self.platform_url}/{username}/?__a=1&__d=1'
+        response = requests.get(url)
+        if response.ok:
+            response = response.json()
+            print(response['graphql']['user']['edge_follow']['count'])    
+        else:
+            self.http_error(response.status_code)
+            
     def get_profile_pic(self,username:str):
         url = f'{self.platform_url}/{username}/?__a=1&__d=1'
         response = requests.get(url)
         if response.ok:
             response = response.json()
             print(response['graphql']['user']['profile_pic_url_hd'])        
-               
+        else:
+            self.http_error(response.status_code)      
       
--- socials/socialmedia.py
+++ socials/socialmedia.py
@@ -9,5 +9,12 @@ class SocialMedia():
     
     def get_platform_name(self):
         return self.platform_name
+    
+    def http_error(self, response_status_code):
+        match response_status_code:
+            case 404:
+                print("Page not found")
+            case _:
+                print("Undefined HTTP error")
 
    
